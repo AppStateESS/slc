1ffdfd35175c54e7ae528e52c5f8deea1bc4ba03
Ted Eberhard <eberhardtm@appstate.edu>
2021-03-26 12:19:39 -0400

    Update slc survey email to use preferred name instead of legal name

--- class/ajax/POSTSendMail.php
+++ class/ajax/POSTSendMail.php
@@ -4,19 +4,25 @@ namespace slc\ajax;
 class POSTSendMail extends AJAX {
 
 	public function execute(){
-
-		$template = array("cName" => $_REQUEST['name']);
-
-		$content = \PHPWS_Template::process($template, 'slc', 'studentSurveyEmail.tpl');
-
 		$banner = $_REQUEST['banner_id'];
 
-		$query = 'SELECT username FROM slc_student_data WHERE id =' . $banner;
+		$query = 'SELECT * FROM slc_student_data WHERE id =' . $banner;
 
 		$db = new \PHPWS_DB();
-		$username = $db->select(null, $query);
-		$username = $username[0]['username'];
+		$student = $db->select(null, $query);
+		$username = $student[0]['username'];
+        $preferred_name = $student[0]['preferred_name'];
+        $last_name = $student[0]['lname'];
+        $first_name = $student[0]['fname'];
+        $name = $first_name." ".$last_name;
+        
+        if(!empty($preferred_name)){
+            $name = $preferred_name." ".$last_name;
+        }
 
+        $template = array("cName" => $name);
+        $content = \PHPWS_Template::process($template, 'slc', 'studentSurveyEmail.tpl');
+        var_dump($content);exit;
 		$message = \Swift_Message::newInstance();
 
 		$message->setSubject('How was your experience with Student Legal Clinic?');
